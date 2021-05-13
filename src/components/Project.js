c6a836d4e0fbb9c8ed4a571290f60d6b279d43ea
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-05-13 15:58:57 -0700

    updated photos

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,10 +6,10 @@ function Project(props) {
         <div className="row border">
             <h1 className="font-weight-bold text-center m-3">{props.projectTitle}</h1>
             <h2 className="text-center">{props.projectSubTitle}</h2>
-            <img class="project-image text-center" src={props.imgURL}></img> 
+            <img class="project-image text-center" alt="" src={props.imgURL}></img> 
             <p className="text-center">{[props.projectDescription]}</p>
-            <a target="_blank" className="btn btn-primary m-1" href={props.deployedAppURL} >View Deployed Application</a>
-            <a target="_blank" class="btn btn-secondary text-center m-1" href={props.githubURL}>View Github Repository</a>
+            <a target="_blank" rel="noreferrer" className="btn btn-primary m-1" href={props.deployedAppURL} >View Deployed Application</a>
+            <a target="_blank" rel="noreferrer" class="btn btn-secondary text-center m-1" href={props.githubURL}>View Github Repository</a>
         </div>
     </div>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 
-import react from 'react';
+import React from 'react';
 import Project from "./Project";
 
 
